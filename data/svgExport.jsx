7d56398c3e447113db49b4f1ba0b8735573ea64e
nevo0309 <nevo19970309@gmail.com.com>
2025-05-31 19:18:23 +0300

    Add heart svg

---
+++ data/svgExport.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+export const heartSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 0 32 32"
+    aria-hidden="true"
+    role="presentation"
+    focusable="false"
+    style={{
+      display: 'block',
+      fill: 'rgba(0, 0, 0, 0.5)',
+      height: 24,
+      width: 24,
+      stroke: 'var(--linaria-theme_palette-icon-primary-inverse)',
+      strokeWidth: 2,
+      overflow: 'visible',
+    }}
+    width={32}
+    height={32}
+  >
+    <path
+      d="M16 28c7-4.73 14-10 14-17a6.98 6.98 0 0 0-7-7c-1.8 0-3.58.68-4.95 2.05L16 8.1l-2.05-2.05a6.98 6.98 0 0 0-9.9 0A6.98 6.98 0 0 0 2 11c0 7 7 12.27 14 17z"
+      stroke="#FFFFFF"
+      fillOpacity={0.5}
+      fill="#000000"
+      strokeWidth="2px"
+    />
+  </svg>
+)
