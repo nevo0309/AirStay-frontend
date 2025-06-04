a14772397bd22f67ac3cf06f1bd7634eab78fa50
theddn <theddn@gmail.com>
2025-06-04 17:31:37 +0300

    Add basic Image gallery

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -1,46 +1,37 @@
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
-import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
-import { loadStay, addStayMsg } from '../store/stay.actions'
+import { loadStay } from "../store/stay.actions"
+import { DetailsImageGallery } from "../cmps/DetailsImageGallery"
 
 export function StayDetails() {
   const { stayId } = useParams()
-  const stay = useSelector(storeState => storeState.stayModule.stay)
+  const stay = useSelector((storeState) => storeState.stayModule.stay)
 
   useEffect(() => {
     loadStay(stayId)
   }, [stayId])
 
-  async function onAddStayMsg(stayId) {
-    try {
-      await addStayMsg(stayId, 'bla bla ' + parseInt(Math.random() * 10))
-      showSuccessMsg(`Stay msg added`)
-    } catch (err) {
-      showErrorMsg('Cannot add stay msg')
-    }
-  }
+  if (!stay) return <div>Loading...</div>
 
   return (
-    <section className="stay-details">
-      <Link to="/stay">Back to list</Link>
-      <h1>Stay Details</h1>
-      {stay && (
-        <div>
-          <h3>{stay.vendor}</h3>
-          <h4>${stay.price}</h4>
-          <pre> {JSON.stringify(stay, null, 2)} </pre>
-        </div>
-      )}
-      <button
-        onClick={() => {
-          onAddStayMsg(stay._id)
-        }}
-      >
-        Add stay msg
-      </button>
+    <section className='stay-details'>
+      <Link to='/'>Back to list</Link>
+
+      <div className='details-content'>
+        <header className='stay-details-header'>
+          <h1 className='stay-details-title'>{stay.name}</h1>
+        </header>
+
+        <DetailsImageGallery images={stay.imgUrls} />
+
+        {/* <div className='stay-details-gallery'>
+            {stay.imgUrls?.map((url, idx) => (
+              <img key={idx} src={url} alt={`Stay ${idx}`} />
+            ))}
+          </div> */}
+      </div>
     </section>
   )
 }
