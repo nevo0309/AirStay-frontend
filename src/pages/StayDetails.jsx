feeae68225866fc3f991ac7a42849ecff8e5fcf8
theddn <theddn@gmail.com>
2025-06-05 16:44:27 +0300

    Fix style to image gallery

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -7,6 +7,7 @@ import { DetailsImageGallery } from "../cmps/DetailsImageGallery"
 
 export function StayDetails() {
   const { stayId } = useParams()
+
   const stay = useSelector((storeState) => storeState.stayModule.stay)
 
   useEffect(() => {
@@ -17,20 +18,22 @@ export function StayDetails() {
 
   return (
     <section className='stay-details'>
-      <Link to='/'>Back to list</Link>
-
+      {/* <Link to='/'>Back to list</Link> */}
       <div className='details-content'>
         <header className='stay-details-header'>
           <h1 className='stay-details-title'>{stay.name}</h1>
         </header>
-
         <DetailsImageGallery images={stay.imgUrls} />
-
-        {/* <div className='stay-details-gallery'>
-            {stay.imgUrls?.map((url, idx) => (
-              <img key={idx} src={url} alt={`Stay ${idx}`} />
+        {/* <div className='details-amenitiess'>
+          <h3>What this place offers</h3>
+          <ul>
+            {stay.amenities?.map((amenity, idx) => (
+              <li key={idx}>
+                <span>{amenity}</span>
+              </li>
             ))}
-          </div> */}
+          </ul>
+        </div> */}
       </div>
     </section>
   )
