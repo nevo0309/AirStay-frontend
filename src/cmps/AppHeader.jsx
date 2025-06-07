ab96302203f68e052ff570693ed49aaf296a189d
ofirbozna <ofirbozn@gmail.com>
2025-06-07 21:10:14 +0300

    Add new filter cmp and render when page scrolled

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,17 +1,37 @@
 import { Link, NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { logout } from '../store/user.actions'
 import { StayFilter } from '../cmps/StayFilter.jsx'
-import { useState } from 'react'
+import { StayFilterClosed } from './StayFilterClosed.jsx'
 import { logoSvg } from '../../data/svgExport.jsx'
 import { humburgerSvg } from '../../data/svgExport.jsx'
 
 
 export function AppHeader() {
   const user = useSelector(storeState => storeState.userModule.user)
+  const [isStayFilterOpen, setIsStayFilterOpen] = useState(true)
   const navigate = useNavigate()
+
+  console.log(isStayFilterOpen)
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      console.log(scrollY)
+
+      if (scrollY > 1) {
+        setIsStayFilterOpen(false);
+      } else {
+        setIsStayFilterOpen(true);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isStayFilterOpen]);
 
   async function onLogout() {
     try {
@@ -24,16 +44,16 @@ export function AppHeader() {
   }
 
   return (
-    <header className="app-header main-container full">
+    <header className={'app-header main-container full ' + (isStayFilterOpen ? '' : 'closed')}>
       <nav>
         <div className='logo'>
-          <NavLink to="/stay" className="/logo">
+          <NavLink to="/" className="/logo">
             {logoSvg}
             <span>airstay</span>
           </NavLink>
         </div>
 
-        <StayFilter />
+        {isStayFilterOpen ? <StayFilter /> : <StayFilterClosed setIsStayFilterOpen={setIsStayFilterOpen} />}
 
         {/* {!user && (
           <NavLink to="login" className="login-link">
@@ -43,11 +63,11 @@ export function AppHeader() {
         <section className='btns flex'>
           <button className='host-guest-btn'>Become a host</button>
           <button className='menue-btn'>
-           {humburgerSvg}
+            {humburgerSvg}
           </button>
         </section>
 
-   
+
 
         {/* {user && (
           <div className="user-info">
---
+++ src/cmps/StayFilterClosed.jsx
@@ -0,0 +1,15 @@
+import set from 'date-fns/set'
+import { searchSvg } from '../../data/svgExport'
+
+export function StayFilterClosed({setIsStayFilterOpen}) {
+
+    return <section className="stay-filter-closed flex align-center" onClick={() => setIsStayFilterOpen(true)}>
+        <div className='flex'>
+            <img src="public/img/filter.png" alt="" />
+            <h1>Anywhere</h1>
+        </div>
+        <h1>Anytime</h1>
+        <h1>Add guests</h1>
+        <button className="search-btn">{searchSvg}</button>
+    </section>
+}
