045502cb6466238ee37a5f92aa79b3311e66c94c
nevo0309 <nevo19970309@gmail.com.com>
2025-06-13 13:56:12 +0300

    Fix img gallery layout

--- src/cmps/details/DetailsImageGallery.jsx
+++ src/cmps/details/DetailsImageGallery.jsx
@@ -2,14 +2,11 @@ export function DetailsImageGallery({ images }) {
   if (!images.length) return null
 
   return (
-    <section className='details-image-gallery'>
-      <div className='grid-container'>
+    <section className="details-image-gallery">
+      <div className="grid-container">
         {images.map((url, idx) => (
-          <div
-            key={idx}
-            className={`image-wrapper ${idx === 0 ? "main-image" : ""}`}
-          >
-            <img src={url} alt={`Stay image ${idx + 1}`} loading='lazy' />
+          <div key={idx} className={`image-wrapper ${idx === 0 ? 'main-image' : ''}`}>
+            <img src={url} alt={`Stay image ${idx + 1}`} loading="lazy" />
           </div>
         ))}
       </div>
