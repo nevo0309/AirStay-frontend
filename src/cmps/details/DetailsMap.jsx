801628812a8990e269a9e9d9dc0b879ba82a4e52
nevo0309 <nevo19970309@gmail.com.com>
2025-06-15 17:08:21 +0300

    Fix layout of the map component

--- src/cmps/details/DetailsMap.jsx
+++ src/cmps/details/DetailsMap.jsx
@@ -5,6 +5,8 @@ import { homeSvg } from '../../../data/svgExport'
 const containerStyle = {
   width: '100%',
   height: '500px',
+  borderRadius: '20px',
+  marginBottom: '5rem',
 }
 const DEFAULT_ZOOM = 16
 
@@ -19,24 +21,30 @@ export function DetailsMap({ stay }) {
   if (!isLoaded) return null
 
   return (
-    <GoogleMap mapContainerStyle={containerStyle} center={stayLoc} zoom={DEFAULT_ZOOM}>
-      <OverlayView position={stayLoc} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
-        <div
-          style={{
-            justifySelf: 'center',
-            transform: 'translate(-50%, -100%)',
-            pointerEvents: 'none',
-            backgroundColor: '#222222',
-            borderRadius: '20px',
-            padding: '10px 10px',
-            boxShadow: '0 2px 10px rgba(0,0,0,0.2)',
-            color: 'white',
-            fontWeight: 'bold',
-          }}
-        >
-          {homeSvg}
-        </div>
-      </OverlayView>
-    </GoogleMap>
+    <section className="details-map">
+      <div className="location-address">
+        <h2>Where you’ll be</h2>
+        <p> {stay.loc.address}</p>
+      </div>
+      <GoogleMap mapContainerStyle={containerStyle} center={stayLoc} zoom={DEFAULT_ZOOM}>
+        <OverlayView position={stayLoc} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
+          <div
+            style={{
+              justifySelf: 'center',
+              transform: 'translate(-50%, -100%)',
+              pointerEvents: 'none',
+              backgroundColor: '#222222',
+              borderRadius: '20px',
+              padding: '10px 10px',
+              boxShadow: '0 2px 10px rgba(0,0,0,0.2)',
+              color: 'white',
+              fontWeight: 'bold',
+            }}
+          >
+            {homeSvg}
+          </div>
+        </OverlayView>
+      </GoogleMap>
+    </section>
   )
 }
