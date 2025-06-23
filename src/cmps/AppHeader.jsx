80ad6b51b9686af01ec594c68eb58da2acc6b98b
nevo0309 <nevo19970309@gmail.com.com>
2025-06-23 19:15:07 +0300

    Add the option to login and signup

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,146 +1,143 @@
-import { Link, NavLink, useLocation } from "react-router-dom"
-import { useNavigate } from "react-router"
-import { useSelector } from "react-redux"
-import { useState, useEffect } from "react"
+import { Link, NavLink, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router'
+import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
 
-import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service"
-import { logout } from "../store/user.actions"
-import { StayFilter } from "../cmps/StayFilter.jsx"
-import { StayFilterClosed } from "./StayFilterClosed.jsx"
-import { logoSvg } from "../../data/svgExport.jsx"
-import { humburgerSvg } from "../../data/svgExport.jsx"
-import { heartSvg } from "../../data/svgExport.jsx"
-import { wishlistSvg } from "../../data/svgExport.jsx"
-import { trips2 } from "../../data/svgExport.jsx"
-import { usePathMatch } from "../customHooks/usePathMatch.js"
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+import { logout } from '../store/user.actions'
+import { StayFilter } from '../cmps/StayFilter.jsx'
+import { StayFilterClosed } from './StayFilterClosed.jsx'
+import { logoSvg } from '../../data/svgExport.jsx'
+import { humburgerSvg } from '../../data/svgExport.jsx'
+import { heartSvg } from '../../data/svgExport.jsx'
+import { wishlistSvg } from '../../data/svgExport.jsx'
+import { trips2 } from '../../data/svgExport.jsx'
+import { usePathMatch } from '../customHooks/usePathMatch.js'
+import { LoginSignup } from '../cmps/LoginSignup.jsx'
+import { Modal } from './Modal.jsx'
 
 export function AppHeader({ isStayFilterOpen, setIsStayFilterOpen }) {
-  const user = useSelector((storeState) => storeState.userModule.user)
+  const user = useSelector(storeState => storeState.userModule.user)
   const [isHosting, setIsHosting] = useState(false)
   const [isSideBarOpen, setIsSideBarOpen] = useState(false)
-  const isFilterSuppressedPage = usePathMatch(
-    ["/hosting/order", "/trips", "/book"],
-    "startsWith"
-  )
-  const isStaticPage = usePathMatch([
-    "/stay",
-    "/hosting",
-    "/trips",
-    "startsWith"
-  ])
+  const isFilterSuppressedPage = usePathMatch(['/hosting/order', '/trips', '/book'], 'startsWith')
+  const isStaticPage = usePathMatch(['/stay', '/hosting', '/trips', 'startsWith'])
+  const [isAuthOpen, setIsAuthOpen] = useState(false)
 
   const navigate = useNavigate()
   const location = useLocation()
 
   useEffect(() => {
-    setIsHosting(location.pathname.startsWith("/hosting/order"))
+    setIsHosting(location.pathname.startsWith('/hosting/order'))
   }, [location.pathname])
 
   useEffect(() => {
-    if (location.pathname.startsWith("/stay")) setIsStayFilterOpen(false)
+    if (location.pathname.startsWith('/stay')) setIsStayFilterOpen(false)
     else setIsStayFilterOpen(true)
   }, [location.pathname])
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY
-      if (location.pathname.startsWith("/stay")) return
+      if (location.pathname.startsWith('/stay')) return
       if (scrollY > 1) {
         setIsStayFilterOpen(false)
       } else {
         setIsStayFilterOpen(true)
       }
     }
-    window.addEventListener("scroll", handleScroll)
+    window.addEventListener('scroll', handleScroll)
 
-    return () => window.removeEventListener("scroll", handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
   }, [isStayFilterOpen, location.pathname])
 
   async function onLogout() {
     try {
       await logout()
-      navigate("/")
+      navigate('/')
       showSuccessMsg(`Bye now`)
     } catch (err) {
-      showErrorMsg("Cannot logout")
+      showErrorMsg('Cannot logout')
     }
   }
   const handleToggle = () => {
-    navigate(isHosting ? "/" : "/hosting/order")
+    navigate(isHosting ? '/' : '/hosting/order')
+  }
+  const onLogInClick = () => {
+    setIsSideBarOpen(!isSideBarOpen)
+    setIsAuthOpen(true)
   }
 
   return (
-    <header
-      className={
-        "app-header main-container full " +
-        (!isStayFilterOpen || isFilterSuppressedPage ? "closed " : "") +
-        (isStaticPage ? " static" : "")
-      }
-    >
-      <nav>
-        <div className='logo'>
-          <NavLink to='/' className='/logo'>
-            {logoSvg}
-            <span>airstay</span>
-          </NavLink>
-        </div>
+    <>
+      <header
+        className={
+          'app-header main-container full ' +
+          (!isStayFilterOpen || isFilterSuppressedPage ? 'closed ' : '') +
+          (isStaticPage ? ' static' : '')
+        }
+      >
+        <nav>
+          <div className="logo">
+            <NavLink to="/" className="/logo">
+              {logoSvg}
+              <span>airstay</span>
+            </NavLink>
+          </div>
 
-        {!isFilterSuppressedPage ? (
-          isStayFilterOpen ? (
-            <StayFilter />
-          ) : (
-            <StayFilterClosed setIsStayFilterOpen={setIsStayFilterOpen} />
-          )
-        ) : null}
+          {!isFilterSuppressedPage ? (
+            isStayFilterOpen ? (
+              <StayFilter />
+            ) : (
+              <StayFilterClosed setIsStayFilterOpen={setIsStayFilterOpen} />
+            )
+          ) : null}
 
-        {/* {!user && (
+          {/* {!user && (
           <NavLink to="login" className="login-link">
           Login
           </NavLink>
           )} */}
 
-        <section className='btns flex'>
-          <button
-            className='host-guest-btn'
-            onClick={(e) => {
-              e.stopPropagation() // Prevent affecting parent click logic
-              handleToggle()
-            }}
-          >
-            {isHosting ? "Switch to traveling" : "Switch to Hosting"}
-          </button>
-          <section className='humburger'>
+          <section className="btns flex">
             <button
-              className='menue-btn'
-              onClick={() => setIsSideBarOpen(!isSideBarOpen)}
+              className="host-guest-btn"
+              onClick={e => {
+                e.stopPropagation() // Prevent affecting parent click logic
+                handleToggle()
+              }}
             >
-              {humburgerSvg}
+              {isHosting ? 'Switch to traveling' : 'Switch to Hosting'}
             </button>
-            {isSideBarOpen && (
-              <section className='humurger-menu flex column'>
-                <p className='flex'>
-                  {wishlistSvg} <span>Wishlist</span>
-                </p>
-                <p className='flex' onClick={() => navigate("/trips")}>
-                  {trips2} <span>Trips</span>
-                </p>
-                <p>Login / Sign up</p>
-              </section>
-            )}
+            <section className="humburger">
+              <button className="menue-btn" onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+                {humburgerSvg}
+              </button>
+              {isSideBarOpen && (
+                <section className="humurger-menu flex column">
+                  <p className="flex">
+                    {wishlistSvg} <span>Wishlist</span>
+                  </p>
+                  <p className="flex" onClick={() => navigate('/trips')}>
+                    {trips2} <span>Trips</span>
+                  </p>
+                  {user ? (
+                    <p onClick={onLogout}>Logout</p>
+                  ) : (
+                    <p onClick={onLogInClick}>Login / Sign up</p>
+                  )}
+                </section>
+              )}
+            </section>
           </section>
-        </section>
-
-        {/* {user && (
-          <div className="user-info">
-            <Link to={`user/${user._id}`}>
-              {user.imgUrl && <img src={user.imgUrl} />}
-              {user.fullname}
-            </Link>
-            <span className="score">{user.score?.toLocaleString()}</span>
-            <button onClick={onLogout}>logout</button>
-          </div>
-        )} */}
-      </nav>
-    </header>
+        </nav>
+      </header>
+      {isAuthOpen && (
+        <Modal onClose={() => setIsAuthOpen(false)}>
+          {/* close after success inside LoginSignup */}
+          <LoginSignup onClose={() => setIsAuthOpen(false)} />
+        </Modal>
+      )}
+    </>
   )
 }
