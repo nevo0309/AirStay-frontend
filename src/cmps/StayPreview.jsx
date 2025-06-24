3047a5d6d43415fdd878f687f88802e7e53ac227
ofirbozna <ofirbozn@gmail.com>
2025-06-24 20:59:29 +0300

    Fix dates on SearchPage

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -4,10 +4,10 @@ import { heartSvg } from '../../data/svgExport.jsx'
 import { ImgCarousel } from './imgCarosel.jsx'
 import { sumNights } from '../services/util.service.js'
 import { useSelector } from 'react-redux'
-
 export function StayPreview({ stay }) {
   const filterBy = useSelector((storeState) => storeState.stayModule.filterBy)
   const location = useLocation()
+
 
   const reviewCount = stay.reviews.reduce(
     (acc, { stars }) => {
@@ -28,8 +28,15 @@ export function StayPreview({ stay }) {
     }
   }
 
+  function formatDate(date) {
+    const options = { month: 'short', day: 'numeric' }
+    const dateToShow = date.toLocaleDateString('en-US', options)
+
+    return dateToShow
+  }
   const totalReview = Object.values(reviewCount).reduce((sum, n) => sum + n, 0)
   const avg = totalReview > 0 ? stay.reviews.reduce((sum, { stars }) => sum + stars, 0) / totalReview : 0
+
   return (
     <article className="preview">
       {location.pathname.startsWith("/search") ?
@@ -39,10 +46,12 @@ export function StayPreview({ stay }) {
 
       <div className="preview-content">
         <h3>{stay.name}</h3>
-        <div className="stay-dates">May 30 – Jun 1</div>
+        {location.pathname.startsWith("/search") ?
+          <div className="stay-dates">{`${formatDate(filterBy.checkIn)}-${formatDate(filterBy.checkOut)}`}</div>
+          : <div className="stay-dates">May 30 – Jun 1</div>}
 
         {location.pathname.startsWith("/search") ?
-          <span className="stay-price-preview"><span>{stay.price}₪ </span>night · <span className='total-price'>{`${sumNights(filterBy.checkIn, filterBy.checkOut) * stay.price}₪  total`}</span></span> :
+          <span className="stay-price-preview"><span>{stay.price}₪ </span>night · <span className='total-price'>{`₪${sumNights(filterBy.checkIn, filterBy.checkOut) * stay.price}  total`}</span></span> :
           <span className="stay-price-preview"><span>{stay.price * 2}₪ </span>· 2 nights</span>}
         <div className='stay-review'><span className='dot'> · </span>★<span>{formatAvg(avg)} <span className='review-count'>{`(${stay.reviews.length})`}</span> </span></div>
 
