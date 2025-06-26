e772fad0fc78ef90418b8659ebb9715101d73e71
ofirbozna <ofirbozn@gmail.com>
2025-06-26 12:01:10 +0300

    Fix bug in stayFilterClosed

--- src/cmps/StayFilterClosed.jsx
+++ src/cmps/StayFilterClosed.jsx
@@ -4,14 +4,14 @@ import { useSelector } from 'react-redux'
 import { useLocation } from 'react-router'
 import { useState } from 'react'
 import { formatCalenderDate } from '../services/util.service'
+import { gu } from 'date-fns/locale'
 
 export function StayFilterClosed({ setIsStayFilterOpen }) {
     const location = useLocation()
     const filterBy = useSelector(storeState => storeState.stayModule.filterBy)
     const where = filterBy.location === 'nearby' ? 'Homes nearby' : `Homes in ${filterBy.location} `
-    const guest = (filterBy.guest.adults + filterBy.guest.children > 1) ?
-        filterBy.guest.adults + filterBy.guest.children + ' guests' :
-        filterBy.guest.adults + filterBy.guest.children + ' guest'
+    const guest = filterBy.guest.adults + filterBy.guest.children 
+    const singlOrPlural = guest > 1 ? ' guests' : ' guest'
     const isSearchPage = location.pathname.startsWith("/search")
 
 
@@ -22,7 +22,7 @@ export function StayFilterClosed({ setIsStayFilterOpen }) {
             <h1> {(filterBy.location && isSearchPage) ? where : 'Anywhere'}</h1>
         </div>
         <h1>{(filterBy.checkIn && filterBy.checkOut && isSearchPage) ? `${formatCalenderDate(filterBy.checkIn)} - ${formatCalenderDate(filterBy.checkOut)}` : 'Anytime'}</h1>
-        <h1>{(guest && isSearchPage) ? guest : 'Add guests'}</h1>
+        <h1>{(guest && isSearchPage) ? (guest + singlOrPlural) : 'Add guests'}</h1>
         <button className="search-btn">{searchSvg}</button>
     </section>
 }
