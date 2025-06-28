83b94e497f616816c6041131ec500c19bd1d79b3
nevo0309 <nevo19970309@gmail.com.com>
2025-06-28 15:17:45 +0300

    Fix error when user is not logged in

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,35 +1,27 @@
-import { Link, NavLink, useLocation } from "react-router-dom"
-import { useNavigate } from "react-router"
-import { useSelector } from "react-redux"
-import { useState, useEffect, useRef } from "react"
+import { Link, NavLink, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router'
+import { useSelector } from 'react-redux'
+import { useState, useEffect, useRef } from 'react'
 
-import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service"
-import { logout } from "../store/user.actions"
-import { StayFilter } from "../cmps/StayFilter.jsx"
-import { StayFilterClosed } from "./StayFilterClosed.jsx"
-import { logoSvg } from "../../data/svgExport.jsx"
-import { humburgerSvg } from "../../data/svgExport.jsx"
-import { heartSvg } from "../../data/svgExport.jsx"
-import { wishlistSvg } from "../../data/svgExport.jsx"
-import { trips2 } from "../../data/svgExport.jsx"
-import { usePathMatch } from "../customHooks/usePathMatch.js"
-import { LoginSignup } from "../cmps/LoginSignup.jsx"
-import { Modal } from "./Modal.jsx"
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+import { logout } from '../store/user.actions'
+import { StayFilter } from '../cmps/StayFilter.jsx'
+import { StayFilterClosed } from './StayFilterClosed.jsx'
+import { logoSvg } from '../../data/svgExport.jsx'
+import { humburgerSvg } from '../../data/svgExport.jsx'
+import { heartSvg } from '../../data/svgExport.jsx'
+import { wishlistSvg } from '../../data/svgExport.jsx'
+import { trips2 } from '../../data/svgExport.jsx'
+import { usePathMatch } from '../customHooks/usePathMatch.js'
+import { LoginSignup } from '../cmps/LoginSignup.jsx'
+import { Modal } from './Modal.jsx'
 
 export function AppHeader({ isStayFilterOpen, setIsStayFilterOpen }) {
   const user = useSelector(storeState => storeState.userModule.user)
   const [isHosting, setIsHosting] = useState(false)
   const [isSideBarOpen, setIsSideBarOpen] = useState(false)
-  const isFilterSuppressedPage = usePathMatch(
-    ["/hosting/order", "/trips", "/book"],
-    "startsWith"
-  )
-  const isStaticPage = usePathMatch([
-    "/stay",
-    "/hosting",
-    "/trips",
-    "startsWith"
-  ])
+  const isFilterSuppressedPage = usePathMatch(['/hosting/order', '/trips', '/book'], 'startsWith')
+  const isStaticPage = usePathMatch(['/stay', '/hosting', '/trips', 'startsWith'])
   const [isAuthOpen, setIsAuthOpen] = useState(false)
 
   const navigate = useNavigate()
@@ -37,14 +29,11 @@ export function AppHeader({ isStayFilterOpen, setIsStayFilterOpen }) {
   const humburgerRef = useRef(null)
 
   useEffect(() => {
-    setIsHosting(location.pathname.startsWith("/hosting/order"))
+    setIsHosting(location.pathname.startsWith('/hosting/order'))
   }, [location.pathname])
 
   useEffect(() => {
-    if (
-      location.pathname.startsWith("/stay") ||
-      location.pathname.startsWith("/search")
-    )
+    if (location.pathname.startsWith('/stay') || location.pathname.startsWith('/search'))
       setIsStayFilterOpen(false)
     else setIsStayFilterOpen(true)
   }, [location.pathname])
@@ -52,30 +41,27 @@ export function AppHeader({ isStayFilterOpen, setIsStayFilterOpen }) {
   useEffect(() => {
     const handleScroll = () => {
       const scrollY = window.scrollY
-      if (location.pathname.startsWith("/stay")) return
+      if (location.pathname.startsWith('/stay')) return
       if (scrollY > 1) {
         setIsStayFilterOpen(false)
-      } else if (!location.pathname.startsWith("/search")) {
+      } else if (!location.pathname.startsWith('/search')) {
         setIsStayFilterOpen(true)
       }
     }
-    window.addEventListener("scroll", handleScroll)
+    window.addEventListener('scroll', handleScroll)
 
-    return () => window.removeEventListener("scroll", handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
   }, [isStayFilterOpen, location.pathname])
 
   useEffect(() => {
     const handleClickOutside = event => {
-      if (
-        humburgerRef.current &&
-        !humburgerRef.current.contains(event.target)
-      ) {
+      if (humburgerRef.current && !humburgerRef.current.contains(event.target)) {
         setIsSideBarOpen(false)
       }
     }
 
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => document.removeEventListener("mousedown", handleClickOutside)
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => document.removeEventListener('mousedown', handleClickOutside)
   }, [])
 
   useEffect(() => {
@@ -85,14 +71,14 @@ export function AppHeader({ isStayFilterOpen, setIsStayFilterOpen }) {
   async function onLogout() {
     try {
       await logout()
-      navigate("/")
+      navigate('/')
       showSuccessMsg(`Bye now`)
     } catch (err) {
-      showErrorMsg("Cannot logout")
+      showErrorMsg('Cannot logout')
     }
   }
   const handleToggle = () => {
-    navigate(isHosting ? "/" : "/hosting/order")
+    navigate(isHosting ? '/' : '/hosting/order')
   }
   const onLogInClick = () => {
     setIsSideBarOpen(!isSideBarOpen)
@@ -103,15 +89,14 @@ export function AppHeader({ isStayFilterOpen, setIsStayFilterOpen }) {
     <>
       <header
         className={
-          "app-header main-container full " +
-          (!isStayFilterOpen || isFilterSuppressedPage ? "closed " : "") +
-          (isStaticPage ? " static" : "")
-        }>
+          'app-header main-container full ' +
+          (!isStayFilterOpen || isFilterSuppressedPage ? 'closed ' : '') +
+          (isStaticPage ? ' static' : '')
+        }
+      >
         <nav>
-          <div className='logo'>
-            <NavLink
-              to='/'
-              className='/logo'>
+          <div className="logo">
+            <NavLink to="/" className="/logo">
               {logoSvg}
               <span>airstay</span>
             </NavLink>
@@ -131,41 +116,38 @@ export function AppHeader({ isStayFilterOpen, setIsStayFilterOpen }) {
           </NavLink>
           )} */}
 
-          <section className='btns flex'>
+          <section className="btns flex">
             <button
-              className='host-guest-btn'
+              className="host-guest-btn"
               onClick={e => {
                 e.stopPropagation() // Prevent affecting parent click logic
                 handleToggle()
-              }}>
-              {isHosting ? "Switch to traveling" : "Switch to Hosting"}
+              }}
+            >
+              {isHosting ? 'Switch to traveling' : 'Switch to Hosting'}
             </button>
             {/* USER AVATAR */}
-            <button className='user-avatar'>
-              <img
-                src={
-                  user?.imgUrl ||
-                  "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
-                }
-                alt={user.fullname}
-              />
-            </button>
-            <section
-              className='humburger'
-              ref={humburgerRef}>
-              <button
-                className='menue-btn'
-                onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+            {user && (
+              <button className="user-avatar">
+                <img
+                  src={
+                    user?.imgUrl ||
+                    'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+                  }
+                  alt={user.fullname}
+                />
+              </button>
+            )}
+            <section className="humburger" ref={humburgerRef}>
+              <button className="menue-btn" onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
                 {humburgerSvg}
               </button>
               {isSideBarOpen && (
-                <section className='humurger-menu flex column'>
-                  <p className='flex'>
+                <section className="humurger-menu flex column">
+                  <p className="flex">
                     {wishlistSvg} <span>Wishlist</span>
                   </p>
-                  <p
-                    className='flex'
-                    onClick={() => navigate("/trips")}>
+                  <p className="flex" onClick={() => navigate('/trips')}>
                     {trips2} <span>Trips</span>
                   </p>
                   {user ? (
