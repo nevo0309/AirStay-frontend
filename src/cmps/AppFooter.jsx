58c1dc64f1a5bb84de46af7b891773cc73ed2cc8
theddn <theddn@gmail.com>
2025-07-01 14:25:47 +0300

    Fix last name text

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,38 +1,52 @@
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux"
 
 export function AppFooter() {
   const count = useSelector(storeState => storeState.userModule.count)
 
   return (
-    <footer className="app-footer main-container full flex">
+    <footer className='app-footer main-container full flex'>
       <section>
-        <footer className="footer flex column">
+        <footer className='footer flex column'>
           <h1>Project developed by:</h1>
           <p className='linkdin flex'>
-            <a href="https://www.linkedin.com/in/ofir-bozna-704857306?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noreferrer">
-              <i className="fa-brands fa-linkedin"></i> Ofir Bozna Nidam
+            <a
+              href='https://www.linkedin.com/in/ofir-bozna-704857306?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app'
+              target='_blank'
+              rel='noreferrer'>
+              <i className='fa-brands fa-linkedin'></i> Ofir Bozna Nidam
             </a>
             |
-            <a href="https://www.linkedin.com/in/nevo-yaakoby-1a9453245?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noreferrer">
-              <i className="fa-brands fa-linkedin"></i> Nevo Yaakoby
+            <a
+              href='https://www.linkedin.com/in/nevo-yaakoby-1a9453245?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app'
+              target='_blank'
+              rel='noreferrer'>
+              <i className='fa-brands fa-linkedin'></i> Nevo Yaakoby
             </a>
             |
-            <a href="http://www.linkedin.com/in/dima-dovgan-479987210" target="_blank" rel="noreferrer">
-              <i className="fa-brands fa-linkedin"></i> Dima Dovgen
+            <a
+              href='http://www.linkedin.com/in/dima-dovgan-479987210'
+              target='_blank'
+              rel='noreferrer'>
+              <i className='fa-brands fa-linkedin'></i> Dima Dovgan
             </a>
           </p>
           <p className='github flex column'>
-            <div className="flex">
-              <a href="https://github.com/nevo0309/AirStay-frontend" target="_blank" rel="noreferrer">
-                <i className="fa-brands fa-github"></i>Frontend
+            <div className='flex'>
+              <a
+                href='https://github.com/nevo0309/AirStay-frontend'
+                target='_blank'
+                rel='noreferrer'>
+                <i className='fa-brands fa-github'></i>Frontend
               </a>
               |
-              <a href="https://github.com/nevo0309/airstay-backend" target="_blank" rel="noreferrer">
-                <i className="fa-brands fa-github"></i> Backend
+              <a
+                href='https://github.com/nevo0309/airstay-backend'
+                target='_blank'
+                rel='noreferrer'>
+                <i className='fa-brands fa-github'></i> Backend
               </a>
             </div>
           </p>
-
         </footer>
       </section>
     </footer>
